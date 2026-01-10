a2ebe0fb3b1d0b11b1ebc56063e577c06db9747c
jonasVOCAST <jonas@vocast.com>
2026-01-10 08:42:19 +0100

    style(styles): Added colors as variants
    Need to implement it everywhere but now it's started

--- client/src/app/shared/components/badge/badge.component.ts
+++ client/src/app/shared/components/badge/badge.component.ts
@@ -36,7 +36,7 @@ export class BadgeComponent {
             case 'info':
                 return 'bg-blue-100 text-blue-800 border-blue-200';
             case 'neutral':
-                return 'bg-gray-100 text-gray-800 border-gray-200';
+                return 'bg-gray-100 text-text-blue border-gray-200';
         }
 
     });
