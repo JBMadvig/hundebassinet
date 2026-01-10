cc66b4840916633af1f86592957bd86362b0f9a6
jonasVOCAST <jonas@vocast.com>
2026-01-10 08:43:44 +0100

    feat(sideBar): Added SidebarPurchaseComponent

--- client/src/app/pages/pos/components/side-bar/sidebar.component.ts
+++ client/src/app/pages/pos/components/side-bar/sidebar.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 
 import { mockUsers } from '../../testdata';
 import { SidebarBasketComponent } from './components/sidebar-basket/sidebar-basket.component';
+import { SidebarPurchaseComponent } from './components/sidebar-purchase/sidebar-purchase.component';
 import { SidebarUserDetailsComponent } from './components/sidebar-user-details/sidebar-user-details.component';
 
 @Component({
     selector: 'app-sidebar',
     imports: [
-        SidebarUserDetailsComponent,
         SidebarBasketComponent,
+        SidebarPurchaseComponent,
+        SidebarUserDetailsComponent,
     ],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.css',
