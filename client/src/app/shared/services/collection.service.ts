a3268a2e43cffeb87640b444b5b4e75f026ced4c
jonasVOCAST <jonas@vocast.com>
2026-01-12 20:33:13 +0100

    feat(collectionService): Added collection service for filtering

---
+++ client/src/app/shared/services/collection.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, signal } from '@angular/core';
+
+import { PrimaryItemCategoriesType } from './../..//shared/types/items.types';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CollectionService {
+
+    public currentFilter = signal<PrimaryItemCategoriesType | 'all'>('all');
+
+    constructor() { }
+}
