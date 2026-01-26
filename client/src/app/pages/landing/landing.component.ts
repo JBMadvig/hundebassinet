cb7f53dc17f9289b679cb5575aee5ee75857eefd
jonasVOCAST <jonas@vocast.com>
2026-01-26 20:59:55 +0100

    fix(landing): Removed virutalKeyboard from template

--- client/src/app/pages/landing/landing.component.ts
+++ client/src/app/pages/landing/landing.component.ts
@@ -2,8 +2,7 @@ import { Component  } from '@angular/core';
 
 @Component({
     selector: 'app-landing',
-    imports: [
-    ],
+    imports: [],
     templateUrl: './landing.component.html',
     styleUrl: './landing.component.css',
 })
