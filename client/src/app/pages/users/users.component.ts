e3eee9ccbeac4fb7befb8fb6ee34c24f88978f15
Jonas Madvig <jonas@vocast.com>
2026-01-27 22:00:24 +0100

    feat(user): Added user page

--- client/src/app/pages/users/users.component.ts
+++ client/src/app/pages/users/users.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
+import { mockUsers } from '../pos/testdata';
+import { DialogComponent } from './../../shared/components/dialog/dialog.component';
+import { User } from './../../shared/types/user.types';
 @Component({
-  selector: 'app-users',
-  imports: [],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.css',
+    selector: 'app-users',
+    imports: [
+        DialogComponent,
+    ],
+    templateUrl: './users.component.html',
+    styleUrl: './users.component.css',
 })
 export class UsersComponent {
+
+    public users = signal<User[]>(mockUsers);
+
+    public selectedUser = signal<User | null>(null);
+
+    public showUserDialog = signal(false);
+
+    public titleModal = computed(() => {
+        const selectedUser = this.selectedUser();
+
+        if (selectedUser) {
+            return `Enter pincode for ${selectedUser.name}`;
+        }
+        return 'Enter pincode';
+    });
 
 }
